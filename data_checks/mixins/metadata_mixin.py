33a0c09dea153bcd07d52b9fb9b9d9031548b8d5
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-07-25 15:17:12 -0700

    update metadata

--- data_checks/mixins/metadata_mixin.py
+++ data_checks/mixins/metadata_mixin.py
@@ -18,15 +18,16 @@ class MetadataMixin:
     def log_metadata(
         self,
         metadata: Dict[str, Any],
+        id: Optional[str] = None,
         write_to_file: Optional[str] = None,
         log_to_memory: bool = True,
     ):
         """
         Log metadata with its associated rule
         """
-        method = inspect.stack()[1][3]
+        metadata_id = inspect.stack()[1][3] if id is None else id
         if log_to_memory:
-            self.metadata[method] = metadata
+            self.metadata[metadata_id] = metadata
         if write_to_file is not None:
             write_metadata(write_to_file, metadata)
 
