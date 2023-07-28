c2d2f125236e6160154cff0748c2c77555f3448c
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-07-28 14:37:14 -0700

    fix bug with field name

--- src/data_checks/check_types.py
+++ src/data_checks/check_types.py
@@ -28,7 +28,7 @@ class CheckBase(ABC):
     description: str
     rules_prefix: str  # Prefix for all rules in the check to be automatically run
     rules: Dict[str, Callable[..., None]]  # Stores all the rules functions in the check
-    rule_params: Dict[
+    rules_params: Dict[
         str,
         FunctionArgs
         | list[FunctionArgs]
