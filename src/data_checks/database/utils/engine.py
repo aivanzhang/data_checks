2b1fb59f25679236739df04a90fb6959e728f4dc
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-08-04 09:54:01 -0700

    add init based off of CHECKS_DATABASE_URL osenv

--- src/data_checks/database/utils/engine.py
+++ src/data_checks/database/utils/engine.py
@@ -1,9 +1,10 @@
 from typing import Optional
+import os
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
 from ..models import Base
 
-engine: Optional[Engine] = None
+engine: Optional[Engine]
 
 Session: Optional[sessionmaker] = None
 
@@ -20,9 +21,9 @@ def _init_database():
         Session = sessionmaker(bind=engine)
 
 
-def start(db_engine: Engine):
-    global engine
-    engine = db_engine
+def start():
+    if engine is None:
+        raise Exception("Engine is not defined")
     _init_database()
 
 
@@ -42,3 +43,8 @@ def get_session() -> sessionmaker:
     if Session is None:
         raise Exception("Session is not defined")
     return Session()
+
+
+check_database_url = os.getenv("CHECKS_DATABASE_URL")
+if check_database_url:
+    connect(check_database_url)
