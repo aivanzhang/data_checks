1d5b41f9f167cfef3a163e24a9bccb7e464b5d13
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-08-10 00:37:45 -0700

    add check_settings

---
+++ tests/check_settings.py
@@ -0,0 +1,7 @@
+import os
+
+CHECKS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASE_URL = "postgresql://ivanzhang:@localhost:5432/test_data_checks_2"
+
+SUITES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
