c4a98f92d5f0eb26fcf340242b04671f1f6a745a
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-08-18 14:03:56 -0700

    store context, and ignore exec_id if not found

--- data_checks/base/actions/check/rule_alerting_action.py
+++ data_checks/base/actions/check/rule_alerting_action.py
@@ -2,6 +2,7 @@
 Action that sends alerts on rule 
 """
 import requests
+import json
 from data_checks.conf.settings import settings
 from data_checks.base.actions.check import CheckAction
 from data_checks.base.check_types import CheckBase
@@ -10,20 +11,34 @@ from data_checks.base.check_types import CheckBase
 class RuleAlertingAction(CheckAction):
     @staticmethod
     def on_success(check: CheckBase, context: dict) -> None:
-        rule_execution_id = context["exec_id"]
+        rule_execution_id = None
+        if "exec_id" in context:
+            rule_execution_id = context["exec_id"]
+
         success_request = requests.post(
             settings["ALERTING_ENDPOINT"],
-            data={"status": "success", "rule_execution_id": rule_execution_id},
+            data={
+                "status": "success",
+                "rule_execution_id": rule_execution_id,
+                "context": json.dumps(context, default=str),
+            },
         )
         if check.verbose:
             print(success_request.status_code, success_request.reason)
 
     @staticmethod
     def on_failure(check: CheckBase, context: dict) -> None:
-        rule_execution_id = context["exec_id"]
+        rule_execution_id = None
+        if "exec_id" in context:
+            rule_execution_id = context["exec_id"]
+
         failed_request = requests.post(
             settings["ALERTING_ENDPOINT"],
-            data={"status": "failure", "rule_execution_id": rule_execution_id},
+            data={
+                "status": "failure",
+                "rule_execution_id": rule_execution_id,
+                "context": json.dumps(context, default=str),
+            },
         )
         if check.verbose:
             print(failed_request.status_code, failed_request.reason)
