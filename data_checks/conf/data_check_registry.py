994ab169f96f8a6454795b087b88680d24525755
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-08-18 14:07:51 -0700

    change data_check_registry

--- data_checks/conf/data_check_registry.py
+++ data_checks/conf/data_check_registry.py
@@ -9,13 +9,18 @@ class DataCheckRegistry:
             raise ImportError("Please specify CHECKS_MODULE in your settings module.")
         else:
             checks = class_utils.classes_for_directory(
-                settings["CHECKS_MODULE"], DataCheck
+                settings["CHECKS_MODULE"],
+                DataCheck,
             )
-            for check in checks:
-                setattr(self, check.__name__, check)
+
+            self.checks: dict[str, type[DataCheck]] = {
+                check.__name__: check for check in checks
+            }
+            self.keys = list(self.checks.keys())
+            self.index = 0
 
     def __getitem__(self, item) -> type:
-        return self.__dict__[item]
+        return self.checks[item]
 
     def __str__(self) -> str:
         return f"{self.__dict__}"
